80e3b0a3668b6f87f2515d34ade85ca8b1ea8f12
LEEDEUKJU <dlemrwn@hallym.ac.kr>
2015-12-31 20:47:24 +0900

    4th

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -1,10 +1,24 @@
 package com.mypcr;
 
+import com.mypcr.emulator.MyPCR;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world3");
+		MyPCR mypcr = new MyPCR();
+		
+		int res = mypcr.add(2, 1);
+		System.out.println(res);
 
+		int res2 = mypcr.sub(2, 1);
+		System.out.println(res2);
+		
+		int res3 = mypcr.mul(2, 1);
+		System.out.println(res3);
+		
+		int res4 = mypcr.div(2, 0);
+		System.out.println(res4);
 	}
 
 }
+
--- src/com/mypcr/emulator/MyPCR.java
+++ src/com/mypcr/emulator/MyPCR.java
@@ -1,5 +1,25 @@
 package com.mypcr.emulator;
 
 public class MyPCR {
-
+	public MyPCR(){
+		
+	}
+	
+	public int add(int a, int b){
+		return a+b;
+	}
+	
+	public int sub(int a, int b){
+		return a-b;
+	}
+	
+	public int mul(int a, int b){
+		return a*b;
+	}
+	
+	public int div(int a, int b){
+		if(b==0) return 0;
+		
+		return a/b;
+	}
 }
